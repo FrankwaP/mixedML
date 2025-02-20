12e0fbea3e2fe9e790bdfe882c8809857d3766e0
François P <francoisplessier@gmail.com>
2025-02-20 09:47:46 +0100

    adding utils for Reservoir Computing

---
+++ R/reservoirR.R
@@ -0,0 +1,47 @@
+# usethis::use_test("reservoirs")
+# library(reticulate)
+
+initiate_reservoirR <- function(spec, data, subject, units, lr, sr, ridge) {
+  stopifnot(class(spec) == "formula")
+  stopifnot(class(data) == "data.frame")
+  stopifnot(class(subject) == "character")
+  stopifnot(class(units) == "numeric")
+  stopifnot(class(lr) == "numeric")
+  stopifnot(class(sr) == "numeric")
+  stopifnot(class(ridge) == "numeric")
+
+  reservoir <- reservoirnet::createNode("Reservoir",
+    units = units,
+    lr = lr,
+    sr = sr
+  )
+  readout <- reservoirnet::createNode("Ridge", ridge = ridge)
+  model <- reservoirnet::link(reservoir, readout)
+  # .GlobalEnv$model <- model
+  return(model)
+}
+
+fit_reservoirR <- function(model, spec, data, subject, pred_rand) {
+  # !!! offsetting is not implemented in LCMM
+  # BUT for linear models, fitting "f(X)+offset" on Y is equivalent to fitting f(X) on "Y-offset"
+  # so that is the method used so far
+  left <- .get_left_side_string(spec)
+  data[left] <- data[left] - pred_rand
+  train_data <- .reshape_for_rnn(spec, data, subject)
+  model <- reservoirnet::reservoirR_fit(
+    model,
+    X = train_data[["X"]],
+    Y = train_data[["Y"]],
+    stateful = FALSE,
+    warmup = 0
+  )
+  # browser()
+  pred_fixed <- reservoirnet::predict_seq(
+    node = model$fit,
+    X = train_data[["X"]],
+    stateful = FALSE
+  )
+  # browser()
+  pred_fixed <- .reshape_pred_of_rnn(pred_fixed, data, subject)
+  return(list("model" = model, "pred_fixed" = pred_fixed))
+}
---
+++ tests/testthat/test-reservoirs.R
@@ -0,0 +1,23 @@
+spec_formula <- Y ~ X1 + X2 + X3
+
+to_scale <- c("X1", "X2", "X3")
+data <- data_hlme
+data[, to_scale] <- scale(data[, to_scale])
+
+
+test_that("reservoir works", {
+  model <- initiate_reservoirR(
+    spec_formula,
+    data,
+    subject = "ID",
+    units = 20,
+    lr = 0.3,
+    sr = 1.1,
+    ridge = 1e-3
+  )
+  pred_rand <- rnorm(nrow(data))
+  fit_result <- fit_reservoirR(model, spec_formula, data, "ID", pred_rand)
+  expect_named(fit_result, c("model", "pred_fixed"))
+  # expect_equal(class(fit_result$model), class(model))
+  expect_vector(fit_result$pred_fixed)
+})
