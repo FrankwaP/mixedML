370719e3d06b0d8d2fc037dd3ba0433a3f7d4f3a
François P <francoisplessier@gmail.com>
2025-05-07 18:28:02 +0200

    testing that the reservoir is really trained

--- tests/testthat/test-reservoirs.R
+++ tests/testthat/test-reservoirs.R
@@ -5,8 +5,13 @@ data <- lcmm::data_hlme
 data[, to_scale] <- scale(data[, to_scale])
 
 
-test_that("esn works", {
-  model <- .initiate_ens(
+data_comp <- data
+data_comp$Y <- data_comp$Y + rnorm(length(data_comp$Y), 1, 2)
+
+pred_rand <- rnorm(nrow(data))
+
+.get_test_model <- function() {
+  return(.initiate_ens(
     fixed_spec = spec_formula,
     subject = "ID",
     esn_controls = esn_ctrls(
@@ -22,9 +27,13 @@ test_that("esn works", {
     ),
     fit_controls = fit_ctrls(warmup = 2),
     predict_controls = predict_ctrls()
-  )
+  ))
+}
 
-  pred_rand <- rnorm(nrow(data))
+
+test_that("esn works", {
+  model <- .get_test_model()
+
   fit_result <- .fit_reservoir(
     model,
     data,
@@ -32,4 +41,15 @@ test_that("esn works", {
   )
   expect_named(fit_result, c("model", "pred_fixed"))
   expect_vector(fit_result$pred_fixed)
+  pred <- .predict_reservoir(fit_result$model, data, "ID")
+  expect(all(pred == fit_result$pred_fixed), 'predictions should be equal')
+  #
+  model_comp <- .get_test_model()
+  fit_result_comp <- .fit_reservoir(
+    model,
+    data_comp,
+    pred_rand
+  )
+  pred_comp <- .predict_reservoir(fit_result_comp$model, data, "ID")
+  expect(all(pred != pred_comp), 'predictions should differ')
 })
